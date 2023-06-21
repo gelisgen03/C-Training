e3cbf07482946ebbfee4afd47db9ac906275c822
enfyna <legendraslan@gmail.com>
2023-06-22 00:32:40 +0300

    Tekran eden kodları sil.

---
+++ saat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// sleep fonksiyonunu kullanarak bir saat yap
+
+int main(){
+    for(int saat = 0; saat < 24; saat++){
+        for(int dakika = 0; dakika < 60; dakika++){
+            for(int saniye = 0; saniye < 60; saniye++){
+                printf("   -> %02d:%02d:%02d <-\r",saat, dakika, saniye);
+                fflush(stdout);
+                sleep(1);
+            };
+        };
+    };
+    return 0;
+};
