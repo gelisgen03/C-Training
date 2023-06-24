441f5e61c0be54a8e767bbf5ca596eb89143fb74
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-06-24 07:51:40 +0300

    Update asal_sayi_bul.c

--- asal_sayi_bul.c
+++ asal_sayi_bul.c
@@ -7,7 +7,7 @@ void main(){
     
     printf("Kontrol edilecek sayi : \n=> ");
     scanf("%d",&a);
-    
+	
     for(int i = 2; i <= a/2; i++){
         if(a % i == 0){
             printf("Asal degil!\n");
@@ -15,12 +15,4 @@ void main(){
         };
     };
     printf("Asal!\n");
-};	
-	
-	
-	
-	
-	
-	
-	
-
+};
